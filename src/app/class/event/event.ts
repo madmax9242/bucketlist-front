61408fed3e6cbc3aad1e3dcaa0d23ce02e89d347
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-06-09 13:52:58 -0400

    only need the damned dist files

--- src/app/class/event/event.ts
+++
@@ -1,9 +0,0 @@
-// This class represents a bucket list item
-export class Event {
-
-	name: string
-	description: string;
-	date: string;
-	type: string;
-	email: string; // foreign key that'll associate users with events
-}
